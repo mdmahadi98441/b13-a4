7f95e1883fa316cdc7908a7e8f8174562eb8e61c
mdmahadi98441-1git config --global user.name mdmahadi98441-1 <mdmahadi98441@gmail.com>
2026-02-24 16:27:13 +0600

    its vary hard

--- script.js
+++ script.js
@@ -1,123 +1,125 @@
-// let interviewCount = 0;
-// let rejectCount = 0;
+//cherch element
+const totalElement = document.getElementById("total");
+const interviewElement = document.getElementById("interview");
+const rejectElement = document.getElementById("reject");
+const jobsText = document.getElementById("jobs");
 
-// const interview = document.getElementById("interview");
-// const reject = document.getElementById("reject");
 
-// const cards = document.querySelectorAll("#card-box");
+//get the btn
+const btnAll = document.getElementById("btn-all-part");
+const btnInterview = document.getElementById('btn-inter-part');
+const btnReject = document.getElementById("btn-reject-part");
 
-// // forEach এর বদলে for...of
-// for (let card of cards) {
+const noJob = document.getElementById("no-job");
 
-//     card.dataset.status = "none";
-
-//     const interviewBtn = card.querySelector(".btn-success");
-//     const rejectBtn = card.querySelector(".btn-error");
-
-//     interviewBtn.addEventListener("click", function () {
-
-//         if (card.dataset.status === "interview") return;
-
-//         if (card.dataset.status === "rejected") {
-//             rejectCount--;
-//         }
-
-//         interviewCount++;
-//         card.dataset.status = "interview";
-
-//         interview.innerText = interviewCount;
-//         reject.innerText = rejectCount;
-//     });
-
-//     rejectBtn.addEventListener("click", function () {
-
-//         if (card.dataset.status === "rejected") return;
-
-//         if (card.dataset.status === "interview") {
-//             interviewCount--;
-//         }
-
-//         rejectCount++;
-//         card.dataset.status = "rejected";
-
-//         interview.innerText = interviewCount;
-//         reject.innerText = rejectCount;
-//     });
-// }
-
+// card
+const cards = document.querySelectorAll('#card-box');
 
 let interviewCount = 0;
 let rejectCount = 0;
-let appliedCount = 0;
+totalElement.innerText = cards.length;
 
-const interviewDisplay = document.getElementById("interview");
-const rejectDisplay = document.getElementById("reject");
-const jobsLeftDisplay = document.getElementById("jobs");
 
-const cards = document.querySelectorAll(".mt-8");
-let totalJobs = cards.length;
+for(const card of cards) {
+    const interviewBtn = card.querySelector("#interview-btn")
+    const rejectBtn= card.querySelector('#reject-btn')
+    const statusBtn = card.querySelector('#select');
 
-jobsLeftDisplay.innerText = totalJobs + " Jobs";
+    card.dataset.status = 'all';
 
-for (let card of cards) {
+    interviewBtn.addEventListener('click',function(){
 
-    card.dataset.status = "none";
-
-    const statusBtn = document.querySelector('button-stutes');
-    const interviewBtn = card.querySelector(".btn-success");
-    const rejectBtn = card.querySelector(".btn-error");
-
-    interviewBtn.addEventListener("click", function () {
-
-        if (card.dataset.status === "interview") return;
-
-        if (card.dataset.status === "none") {
-            appliedCount++;
-            totalJobs--;
+        if (card.dataset.status === "reject") {
+            rejectCount--
         }
 
-        if (card.dataset.status === "rejected") {
-            rejectCount--;
+        if (card.dataset.status !== "interview") {
+            interviewCount++;
         }
 
-        interviewCount++;
-        card.dataset.status = "interview";
+        card.dataset.status = "interview"
+        statusBtn.innerText = "Interview"
+        updateCount();
+    })
 
-        // Status Button Update
-        statusBtn.innerText = "Interview";
-        statusBtn.classList.remove("btn-error");
-        statusBtn.classList.add("btn-success");
 
-        updateUI();
-    });
-
-    rejectBtn.addEventListener("click", function () {
-
-        if (card.dataset.status === "rejected") return;
-
-        if (card.dataset.status === "none") {
-            appliedCount++;
-            totalJobs--;
-        }
-
-        if (card.dataset.status === "interview") {
+    rejectBtn.addEventListener('click',function(){
+        if (card.dataset.status === 'interview') {
             interviewCount--;
         }
 
-        rejectCount++;
-        card.dataset.status = "rejected";
-
-        // Status Button Update
+        if (card.dataset.status !== 'reject') {
+            rejectCount++;
+        }
+        card.dataset.status = "reject";
         statusBtn.innerText = "Rejected";
-        statusBtn.classList.remove("btn-success");
-        statusBtn.classList.add("btn-error");
+        updateCount();
+    })
 
-        updateUI();
-    });
+
+
+}
+function updateCount() {
+    interviewElement.innerText = interviewCount;
+    rejectElement.innerText = rejectCount;
 }
 
-function updateUI() {
-    interviewDisplay.innerText = interviewCount;
-    rejectDisplay.innerText = rejectCount;
-    jobsLeftDisplay.innerText = totalJobs + " Jobs";
+
+btnAll.addEventListener('click',function(){
+    let count = 0
+
+    for (let card of cards) {
+        card.style.display = "block";
+        count++;
+    }
+    jobsText.innerText = count + " Jobs";
+    cheekNoJob(count);
+})
+
+
+btnInterview.addEventListener("click",function(){
+
+    let count = 0;
+    for (let card of cards) {
+
+        if (card.dataset.status === "interview") {
+            card.style.display = "block";
+            count++;
+        } else {
+            card.style.display = "none";
+        }
+    }
+
+    jobsText.innerText = count + " Jobs";
+    cheekNoJob(count);
+})
+
+
+btnReject.addEventListener("click",function(){
+    let count = 0;
+
+    for (let card of cards){
+      if (card.dataset.status === "reject"){
+            card.style.display =  'block'
+            count++;
+        } else {
+            card.style.display ='none'
+        }
+    }
+
+    jobsText.innerText = count + ' Jobs'
+    cheekNoJob(count);
+})
+
+
+
+function cheekNoJob(count) {
+    if (count === 0) {
+        noJob.style.display = 'block'
+    } else {
+        noJob.style.display = 'none'
+    }
 }
+
+// no job hide in fast
+noJob.style.display = 'none'
